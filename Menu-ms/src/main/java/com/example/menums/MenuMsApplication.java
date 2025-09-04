c3d9c0ae947a57c1b968215f631f2cb056b7ac67
Kamil Harunov <harunov21@gmail.com>
2025-09-04 16:54:19 +0400

    new changes commited

---
+++ Menu-ms/src/main/java/com/example/menums/MenuMsApplication.java
@@ -0,0 +1,13 @@
+package com.example.menums;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenuMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MenuMsApplication.class, args);
+	}
+
+}
---
+++ Menu-ms/src/test/java/com/example/menums/MenuMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.menums;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenuMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
